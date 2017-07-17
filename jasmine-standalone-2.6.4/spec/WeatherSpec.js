3b6d2599f39e529bdb9f69cd5123335f16189399
Linton Mackereth <linton@Lintons-MBP.home>
2017-07-17 20:34:39 +0100

    weather test added

---
+++ jasmine-standalone-2.6.4/spec/WeatherSpec.js
@@ -0,0 +1,18 @@
+describe('Weather', function(){
+    var weather
+
+    beforeEach(function() {
+        weather = new Weather()
+    })
+
+    it('can be stormy', function(){
+        spyOn(Math, 'random').and.returnValue(0)
+
+        expect(weather.isStormy()).toBeTruthy()
+    })
+    it('can be nice', function(){
+        spyOn(Math, 'random').and.returnValue(1)
+
+        expect(weather.isStormy()).toBeFalsy()
+    })
+})
---
+++ jasmine-standalone-2.6.4/src/weather.js
@@ -0,0 +1,9 @@
+function Weather() {
+    this._NOT_STORMY_PROBABILITY = 0.5
+}
+
+Weather.prototype = {
+     isStormy: function () {
+         return (Math.random() < this._NOT_STORMY_PROBABILITY) 
+     }
+}
